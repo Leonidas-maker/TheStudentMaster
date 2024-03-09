dc2736ea3704368c5d84b05ce8506b244855dfff
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2024-03-09 18:47:57 +0100

    Model iCal-Data added

--- Server/models/m_ical.py
+++ Server/models/m_ical.py
@@ -1,28 +1,67 @@
 from sqlalchemy import Column, Integer, String, TIMESTAMP, BOOLEAN, ForeignKey
-from sqlalchemy.dialects.mysql import MEDIUMBLOB
 from config.database import Base
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
 
 class ICalGeneral(Base):
     __tablename__ = "ical_general"
 
     ical_id = Column(Integer, primary_key=True, index=True, nullable=False)
+
+    name = Column(String(255), nullable=False)
+    source = Column(String(255), nullable=False, unique=True)
+
     is_active = Column(BOOLEAN, nullable=False, default=False)
     is_custom = Column(BOOLEAN, nullable=False, default=False)
-
-class ICalDHBWMannheim(Base):
-    __tablename__ = "ical_dhbw_mannheim"
-
-    ical_id = Column(Integer, ForeignKey("ical_general.ical_id"), nullable=False, primary_key=True)
-    ical_source_id = Column(Integer, nullable=False, unique=True)
-    name = Column(String(255), nullable=False)
-    ical_data = Column(MEDIUMBLOB(204800), nullable=False)
     last_modified = Column(TIMESTAMP, nullable=False)
 
-class ICalCustom(Base):
-    __tablename__ = "ical_custom"
+    # Mapped is a type hint for the 1:n relationship
+    events: Mapped[List["ICalEventBase"]] = relationship(back_populates="ical")
 
-    ical_id = Column(Integer, ForeignKey("ical_general.ical_id"), nullable=False, primary_key=True)
-    source_url = Column(String(255), nullable=False, unique=True)
-    name = Column(String(255), nullable=False)
-    ical_data = Column(MEDIUMBLOB(204800), nullable=False)
+
+class ICalEventBase(Base):
+    __tablename__ = "ical_event_base"
+
+    base_event_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    ical_id = Column(Integer, ForeignKey("ical_general.ical_id"), nullable=False)
+
+    summary = Column(String(255), nullable=False)
+    last_modified = Column(TIMESTAMP, nullable=False)
+
+    event = relationship("ICalEvent", backref="ical_event_base")
+    ical = relationship("ICalGeneral", backref="ical_event_base")
+
+
+class ICalEvent(Base):
+    __tablename__ = "ical_event"
+
+    event_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    base_event_id = Column(Integer, ForeignKey("ical_event_base.base_event_id"), nullable=False)
+
+    description = Column(String(255))
+    location_id = Column(Integer, ForeignKey("ical_location.location_id"), nullable=False)
+
+    start = Column(TIMESTAMP, nullable=False)
+    end = Column(TIMESTAMP, nullable=False)
+    timezone_id = Column(Integer, ForeignKey("time_zones.timezone_id"), nullable=False)
+
+    last_modified = Column(TIMESTAMP, nullable=False)
+
+    location = relationship("ICalLocation", backref="ical_event")
+    timezone = relationship("TimeZones", backref="ical_event")
+
+
+class ICalLocation(Base):
+    __tablename__ = "ical_location"
+
+    location_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    location = Column(String(255), nullable=False)
+    last_modified = Column(TIMESTAMP, nullable=False)
+
+
+class TimeZones(Base):
+    __tablename__ = "time_zones"
+
+    timezone_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    timezone = Column(String(255), nullable=False)
     last_modified = Column(TIMESTAMP, nullable=False)
