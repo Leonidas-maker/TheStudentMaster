db84040aec7216336fb705de4dc3da3e82b1256c
Schuetze1000 <schuetzeandreas.1@web.de>
2024-03-11 18:14:42 +0100

    Recreated Frontend with Nativewind v4 -Now working dark mode

--- Client/App.tsx
+++
@@ -1,9 +0,0 @@
-import App from './src/App';
-export default App;
-export * from './src/App';
-
-import { NativeWindStyleSheet } from "nativewind";
-
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
--- Client/src/components/navigator/ModuleNavigator.tsx
+++ Client/src/components/navigator/ModuleNavigator.tsx
@@ -1,13 +1,8 @@
-import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
-import { styled } from "nativewind";
 import { useTranslation } from "react-i18next";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
 
-const StyledText = styled(Text);
-const StyledView = styled(View);
-const StyledTouchableOpacity = styled(TouchableOpacity);
 
 function ModuleNavigator() {
     const navigation = useNavigation<any>();
@@ -31,58 +26,58 @@ function ModuleNavigator() {
     const { t } = useTranslation();
 
     return (
-        <StyledView className="m-4">
-            <StyledText className="text-font_primary text-xl font-bold mb-2">More Modules</StyledText>
-            <StyledView className="bg-secondary rounded-lg shadow-md p-4 border border-gray-700">
-                <StyledTouchableOpacity
+        <View className="m-4">
+            <Text className="text-font_primary text-xl font-bold mb-2">More Modules</Text>
+            <View className="bg-secondary rounded-lg shadow-md p-4 border border-gray-700">
+                <TouchableOpacity
                     onPress={handleDashboardPress}
                 >
-                    <StyledView className="flex-row justify-between items-center">
-                        <StyledView className="flex-row items-center">
+                    <View className="flex-row justify-between items-center">
+                        <View className="flex-row items-center">
                             <Icon name="dashboard" size={20} color="#E0E0E2" />
-                            <StyledText className="text-font_primary font-bold text-lg ml-2">Dashboard</StyledText>
-                        </StyledView>
+                            <Text className="text-font_primary font-bold text-lg ml-2">Dashboard</Text>
+                        </View>
                         <Icon name="arrow-forward-ios" size={20} color="#E0E0E2" />
-                    </StyledView>
-                </StyledTouchableOpacity>
-                <StyledView className="border-b border-gray-700 my-2" />
-                <StyledTouchableOpacity
+                    </View>
+                </TouchableOpacity>
+                <View className="border-b border-gray-700 my-2" />
+                <TouchableOpacity
                     onPress={handleDualisPress}
                 >
-                    <StyledView className="flex-row justify-between items-center">
-                        <StyledView className="flex-row items-center">
+                    <View className="flex-row justify-between items-center">
+                        <View className="flex-row items-center">
                             <Icon name="school" size={20} color="#E0E0E2" />
-                            <StyledText className="text-font_primary font-bold text-lg ml-2">Dualis</StyledText>
-                        </StyledView>
+                            <Text className="text-font_primary font-bold text-lg ml-2">Dualis</Text>
+                        </View>
                         <Icon name="arrow-forward-ios" size={20} color="#E0E0E2" />
-                    </StyledView>
-                </StyledTouchableOpacity>
-                <StyledView className="border-b border-gray-700 my-2" />
-                <StyledTouchableOpacity
+                    </View>
+                </TouchableOpacity>
+                <View className="border-b border-gray-700 my-2" />
+                <TouchableOpacity
                     onPress={handleFlashcardsPress}
                 >
-                    <StyledView className="flex-row justify-between items-center">
-                        <StyledView className="flex-row items-center">
+                    <View className="flex-row justify-between items-center">
+                        <View className="flex-row items-center">
                             <Icon name="style" size={20} color="#E0E0E2" />
-                            <StyledText className="text-font_primary font-bold text-lg ml-2">Flashcards</StyledText>
-                        </StyledView>
+                            <Text className="text-font_primary font-bold text-lg ml-2">Flashcards</Text>
+                        </View>
                         <Icon name="arrow-forward-ios" size={20} color="#E0E0E2" />
-                    </StyledView>
-                </StyledTouchableOpacity>
-                <StyledView className="border-b border-gray-700 my-2" />
-                <StyledTouchableOpacity
+                    </View>
+                </TouchableOpacity>
+                <View className="border-b border-gray-700 my-2" />
+                <TouchableOpacity
                     onPress={handleMealPlanPress}
                 >
-                    <StyledView className="flex-row justify-between items-center">
-                        <StyledView className="flex-row items-center">
+                    <View className="flex-row justify-between items-center">
+                        <View className="flex-row items-center">
                             <Icon name="restaurant" size={20} color="#E0E0E2" />
-                            <StyledText className="text-font_primary font-bold text-lg ml-2">Meal Plan</StyledText>
-                        </StyledView>
+                            <Text className="text-font_primary font-bold text-lg ml-2">Meal Plan</Text>
+                        </View>
                         <Icon name="arrow-forward-ios" size={20} color="#E0E0E2" />
-                    </StyledView>
-                </StyledTouchableOpacity>
-            </StyledView>
-        </StyledView>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
     );
 }
 
