cb2e3ba6ae8a4e201444d0c3ab12c956d01a84db
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2024-04-02 13:22:31 +0200

    changed event styling, added event json import, added event overlap function

--- Client/src/components/calendar/Event.tsx
+++ Client/src/components/calendar/Event.tsx
@@ -1,46 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import 'nativewind';
 
 interface EventProps {
     event: {
-        id: number;
-        title: string;
-        description?: string;
+        summary: string;
+        description?: string | null;
+        location?: string;
         start: Date;
         end: Date;
     },
     hoursContainerHeight: number;
     containerHeight: number;
-    calender: {
+    calendar: {
         startHour: number;
         endHour: number;
-    }
+    },
+    overlapCount?: number;
+    overlapIndex?: number;
 }
 
-const Event: React.FC<EventProps> = ({ event, hoursContainerHeight, containerHeight, calender }) => {
+//TODO Implement all day events
+const Event: React.FC<EventProps> = ({ event, hoursContainerHeight, containerHeight, calendar, overlapCount = 1, overlapIndex = 0 }) => {
     let eventStart: number, eventEnd: number, eventDuration: number;
 
     // Event calculation for same day event
     if (event.start.getDate() === event.end.getDate()) {
         // Event start calculation in hours minus the start hour of the calender
-        eventStart = event.start.getHours() + (event.start.getMinutes() / 60) - calender.startHour;
+        eventStart = event.start.getHours() + (event.start.getMinutes() / 60) - calendar.startHour;
         // Event end calculation in hours minus the start hour of the calender
-        eventEnd = event.end.getHours() + (event.end.getMinutes() / 60) - calender.startHour;
+        eventEnd = event.end.getHours() + (event.end.getMinutes() / 60) - calendar.startHour;
         // Event duration calculation
         eventDuration = eventEnd - eventStart;
     }
     else {
         // TODO Implement event spanning multiple days
-        eventStart = event.start.getHours() + (event.start.getMinutes() / 60) - calender.startHour;
-        eventEnd = event.end.getHours() + (event.end.getMinutes() / 60) - calender.startHour;
+        eventStart = event.start.getHours() + (event.start.getMinutes() / 60) - calendar.startHour;
+        eventEnd = event.end.getHours() + (event.end.getMinutes() / 60) - calendar.startHour;
         eventDuration = eventEnd - eventStart;
     }
 
     // Calculates the difference between the day container and the hours container
     const dayHeight = containerHeight - hoursContainerHeight;
     // Calculates the total hours displayed in the calender
-    const totalHours = calender.endHour - calender.startHour;
+    const totalHours = calendar.endHour - calendar.startHour;
     // Calculates the height of 1 hour
     const hourHeight = hoursContainerHeight / totalHours;
     // Calculates the top position of the event with event start time times the hour height 
@@ -49,19 +52,26 @@ const Event: React.FC<EventProps> = ({ event, hoursContainerHeight, containerHei
     // Calculates the total height of the event with the event duration times the hour height
     const eventHeight = eventDuration * hourHeight;
 
+    const handleEventPress = () => {
+        console.log(event);
+    };
+
+    const eventWidth = 100 / overlapCount;
+    const leftPosition = (100 / overlapCount) * overlapIndex;
+
     return (
-        <View 
-        className='bg-blue-500'
-        style={{
-            position: 'absolute',
-            top: topPosition,
-            width: '100%',
-            height: eventHeight,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-            <Text className='text-white'>{event.title}</Text>
-        </View>
+        <TouchableOpacity
+            onPress={handleEventPress}
+            className='bg-blue-500 rounded-lg shadow-sm'
+            style={{
+                position: 'absolute',
+                top: topPosition,
+                left: `${leftPosition}%`,
+                width: `${eventWidth}%`,
+                height: eventHeight,
+            }}>
+            <Text className='text-white pt-2 px-1'>{event.summary}</Text>
+        </TouchableOpacity>
     );
 };
 
