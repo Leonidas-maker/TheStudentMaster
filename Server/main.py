0cb052922492ebe89892694ae064cd149fcb3d93
chillkroete <info@wolf-andreas.eu>
2024-04-04 12:06:14 +0200

    Add Canteen Menu API Endpoints

--- Server/main.py
+++ Server/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Depends, BackgroundTasks
 from fastapi_cdn_host import monkey_patch_for_docs_ui
-from fastapi_utils.tasks import repeat_every
+# from fastapi_utils.tasks import repeat_every
 from contextlib import asynccontextmanager
 from sqlalchemy.orm import Session
 import asyncio
+import json
 
 # ~~~~~~~~~~~~~~~~~ Config ~~~~~~~~~~~~~~~~ #
 from config.database import engine
@@ -12,7 +13,12 @@ from config.database import engine
 from middleware.database import get_async_db, get_db
 from middleware.general import create_address
 from middleware.ical import update_all_ical_dhbw_mannheim
-from middleware.canteen import create_canteen, canteen_menu_to_db, create_canteens, update_canteen_menus
+from middleware.canteen import (
+    create_canteen,
+    canteen_menu_to_db,
+    create_canteens,
+    update_canteen_menus,
+)
 
 # ~~~~~~~~~~~~~~~~ Schemas ~~~~~~~~~~~~~~~~ #
 from models.pydantic_schemas import s_general
@@ -39,12 +45,49 @@ m_canteen.Base.metadata.create_all(bind=engine)
 async def lifespan(app: FastAPI):
     # ~~~~~~~~~ Code to run on startup ~~~~~~~~ #
     async with get_async_db() as db:
-        await asyncio.to_thread(create_canteens, db)
+        with open("./utils/canteen/canteen_addresses.json", "r") as file:
+            canteens = json.load(file)
+        for canteen_obj in canteens:
+            # create address
+            address_new = s_general.AddressCreate(
+                address1=canteen_obj["address1"],
+                address2=canteen_obj["address2"] if "address2" in canteen_obj else None,
+                district=canteen_obj["district"],
+                postal_code=canteen_obj["postal_code"],
+                city=canteen_obj["city"],
+                country=canteen_obj["country"],
+            )
+            address_new = create_address(db, address_new)
+            # create canteen
+            canteen_new = m_canteen.Canteen(
+                canteen_name=canteen_obj["name"],
+                canteen_short_name=(
+                    canteen_obj["short_name"] if "short_name" in canteen_obj else None
+                ),
+                address_id=address_new.address_id,
+            )
+            create_canteen(db, canteen_new)
+        db.commit()
+
+    # update all canteen menus
+    async with get_async_db() as db:
+        canteens = db.query(m_canteen.Canteen).all()
+        for canteen_obj in canteens:
+            # TODO loading bar
+            for i in range(3):
+                canteen_menu_to_db(
+                    db=db, canteen_id=canteen_obj.canteen_id, week_offset=i
+                )
+        db.commit()
+    # ~~~~~~~~ End of code to run on startup ~~~~~~~~ #
     yield
     # ~~~~~~~~ Code to run on shutdown ~~~~~~~~ #
 
+    # ~~~~~~~~ End of code to run on shutdown ~~~~~~~~ #
+    pass
 
-app = FastAPI(lifespan=lifespan)
+
+app = FastAPI(lifespan=lifespan, swagger_ui_parameters={"operationsSorter": "tag"})
 monkey_patch_for_docs_ui(app)
 
 
@@ -53,9 +96,9 @@ monkey_patch_for_docs_ui(app)
 # ======================================================== #
 
 
-@repeat_every(seconds=60 * 15)  # 15 minutes
-def update_Data(db: Session = Depends(get_db)):
-    update_all_ical_dhbw_mannheim(db)
+# @repeat_every(seconds=60 * 15)  # 15 minutes
+# def update_Data(db: Session = Depends(get_db)):
+#     update_all_ical_dhbw_mannheim(db)
     # update_canteen_menus(db)
 
 
@@ -72,6 +115,6 @@ async def root(db: Session = Depends(get_db)):
 # ======================== Router ======================== #
 # ======================================================== #
 
-app.include_router(user.users_router, prefix="/user")
-app.include_router(auth.auth_router, prefix="/auth")
-app.include_router(canteen.canteen_router, prefix="/canteen")
+app.include_router(user.users_router, prefix="/user", tags=["user"])
+app.include_router(auth.auth_router, prefix="/auth", tags=["auth"])
+app.include_router(canteen.canteen_router, prefix="/canteen", tags=["canteen"])
--- Server/routes/canteen.py
+++ Server/routes/canteen.py
@@ -4,12 +4,16 @@ from sqlalchemy.orm import Session
 
 
 # ~~~~~~~~~~~~~~~~~ Models ~~~~~~~~~~~~~~~~ #
-from models.pydantic_schemas.s_canteen import ResGetCanteen, ResGetCanteenAddress
+from models.pydantic_schemas.s_canteen import (
+    ResGetCanteen,
+    ResGetCanteenAddress,
+    ResGetCanteenMenu,
+    ResGetCanteenMenuDay,
+)
 from models.sql_models import m_canteen
 
 # ~~~~~~~~~~~~~~~ Middleware ~~~~~~~~~~~~~~ #
 from middleware.database import get_db
-
 
 
 ###########################################################################
@@ -18,12 +22,23 @@ from middleware.database import get_db
 
 canteen_router = APIRouter()
 
+# ======================================================== #
+# ======================== Canteen ======================= #
+# ======================================================== #
+
+@canteen_router.get("/all", response_model=list[ResGetCanteen])
+def canteen_read_all(db: Session = Depends(get_db)) -> list[dict]:
+    return [canteen.as_dict() for canteen in db.query(m_canteen.Canteen).all()]
+
+
 @canteen_router.get("/{canteen_id}", response_model=ResGetCanteen)
 def canteen_read(
     canteen_id: Annotated[int, "The ID of the canteen to retrieve."],
     db: Session = Depends(get_db),
 ):
-    return db.query(m_canteen.Canteen).filter_by(canteen_id=canteen_id).first().as_dict()
+    return (
+        db.query(m_canteen.Canteen).filter_by(canteen_id=canteen_id).first().as_dict()
+    )
 
 
 @canteen_router.get("/{canteen_id}/address", response_model=ResGetCanteenAddress)
@@ -31,7 +46,51 @@ def canteen_read_all_details(
     canteen_id: Annotated[int, "The ID of the canteen to retrieve."],
     db: Session = Depends(get_db),
 ):
-    return db.query(m_canteen.Canteen).filter_by(canteen_id=canteen_id).first().as_dict_complete()
+    return (
+        db.query(m_canteen.Canteen)
+        .filter_by(canteen_id=canteen_id)
+        .first()
+        .as_dict_complete()
+    )
+
+# ======================================================== #
+# ===================== Canteen Menu ===================== #
+# ======================================================== #
+
+@canteen_router.get("/{canteen_id}/menu/all", response_model=list[ResGetCanteenMenu])
+def canteen_read_menu_all(
+    canteen_id: Annotated[int, "The ID of the canteen to retrieve the menu for."],
+    db: Session = Depends(get_db),
+) -> list[dict]:
+    return [
+        menu.as_dict()
+        for menu in db.query(m_canteen.Menu).filter_by(canteen_id=canteen_id).all()
+    ]
+
+
+@canteen_router.get("/menu/{day}", response_model=list[ResGetCanteenMenuDay])
+def canteen_read_menu_day(
+    day: Annotated[str, "The day to retrieve the menu for. (e.g. '2021-10-01')"],
+    db: Session = Depends(get_db),
+) -> list[dict]:
+    return_value = []
+    for menu in db.query(m_canteen.Menu).filter_by(serving_date=day).all():
+        menu_row = dict()
+        menu_row["canteen_name"] = menu.canteen.canteen_name
+        menu_row["canteen_short_name"] = (
+            menu.canteen.canteen_short_name if menu.canteen.canteen_short_name else None
+        )
+        menu_row["image_url"] = (
+            menu.canteen.image_url if menu.canteen.image_url else None
+        )
+        menu_row["menu"] = {
+            "dish_type": menu.dish_type,
+            "dish": menu.dish.description,
+            "price": menu.dish.price,
+            "serving_date": menu.serving_date,
+        }
+        return_value.append(menu_row)
+    return return_value
 
 
 # TODO: not finished
