f5df6cafa60a48a6de40b307ee6cb6314affa098
Schuetze1000 <schuetzeandreas.1@web.de>
2024-04-05 14:31:46 +0200

    Added rich progressbar for ech repeated task

--- Server/middleware/ical.py
+++ Server/middleware/ical.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import Session
-from tqdm import tqdm
 
 # ~~~~~~~~~~~~~~~~~ Models ~~~~~~~~~~~~~~~~ #
 from models.sql_models.m_ical import ICalCustom, ICalDHBWMannheim
@@ -11,64 +10,72 @@ from utils.ical.general import get_ical_data, get_icals_data
 # ======================================================== #
 # ===================== ICal Updater ===================== #
 # ======================================================== #
-def update_ical_custom(db: Session):
-    icals = db.query(ICalCustom).all()
-
+def update_ical_custom(db: Session, progress, task_id):
+    icals = db.query(ICalCustom).all() 
+    progress.update(task_id, total=len(icals), refresh=True)
     for ical in icals:
+        progress.update(task_id, description=f"[bold green]iCal-Custom[/bold green] Update {ical.name}")
         ical_data, ical_hash = get_ical_data(ical.source_url, "iCal-Custom")
         if ical_hash != ical.hash:
             ical.data = ical_data
             ical.hash = ical_hash
             db.add(ical)
+        progress.update(task_id, advance=1)
     db.commit()
 
 
-def update_ical_dhbw_mannheim(db: Session, only_active: bool = True):
+def update_ical_dhbw_mannheim(db: Session, progress, task_id, only_active: bool = True):
     if only_active:
         icals = db.query(ICalDHBWMannheim).filter(ICalDHBWMannheim.is_active == True).all()
     else:
         icals = db.query(ICalDHBWMannheim).all()
 
-    progress = tqdm(icals, leave=False, total=len(icals), ascii=" ▖▘▝▗▚▞█")
+    progress.update(task_id, total=len(icals), refresh=True)
+
     for ical in icals:
-        progress.set_description(f"[iCal-DHBWMannheim] Update {ical.name}")
+        progress.update(task_id, description=f"[bold green]iCal-DHBWMannheim[/bold green] Update {ical.name}")
         ical_data, ical_hash = get_ical_data(ical.source, "iCal-DHBWMannheim")
         if ical_hash != ical.hash:
             ical.data = ical_data
             ical.hash = ical_hash
             db.add(ical)
-        progress.update(1)
+        progress.update(task_id, advance=1)
         
-    progress.close()
     db.commit()
 
+def update_all_ical_dhbw_mannheim(db: Session, progress, task_ids:tuple):
+    try:
+        icals = db.query(ICalDHBWMannheim).all()
+        available_sources = nativ_sources.get_source_dhbw_ma()
+        progress.update(task_ids[0], total=len(icals), refresh=True)
+        
+        for i, ical in enumerate(icals):
+            progress.update(task_ids[0], description=f"[bold green]iCal-DHBWMannheim[/bold green] Update {ical.name}")
+            if ical.source not in available_sources.values():
+                db.delete(ical)
+            else:
+                available_sources.pop(ical.name, None)
+                ical_data, ical_hash = get_ical_data(ical.source, "iCal-DHBWMannheim")
+                if ical_hash != ical.hash:
+                    ical.data = ical_data
+                    ical.hash = ical_hash
+                    db.add(ical)
+            progress.update(task_ids[0], advance=1)
 
-def update_all_ical_dhbw_mannheim(db: Session):
-    icals = db.query(ICalDHBWMannheim).all()
-    available_sources = nativ_sources.get_source_dhbw_ma()
-    
-    progress = tqdm(icals, leave=False, total=len(icals), ascii=" ▖▘▝▗▚▞█")
-    for ical in icals:
-        progress.set_description(f"[iCal-DHBWMannheim] Update {ical.name}")
-        if ical.source not in available_sources.values():
-            db.delete(ical)
-        else:
-            available_sources.pop(ical.name, None)
-            ical_data, ical_hash = get_ical_data(ical.source, "iCal-DHBWMannheim")
-            if ical_hash != ical.hash:
-                ical.data = ical_data
-                ical.hash = ical_hash
-                db.add(ical)
-        progress.update(1)
-    progress.close()
-    
-    progress = tqdm(available_sources.items(), leave=False, total=len(available_sources), ascii=" ▖▘▝▗▚▞█")
-    for name, source in available_sources.items():
-        progress.set_description(f"[iCal-DHBWMannheim] Adding {name}")
-        ical_data, ical_hash = get_ical_data(source, "iCal-DHBWMannheim")
-        ical = ICalDHBWMannheim(name=name, source=source, data=ical_data, hash=ical_hash)
-        db.add(ical)
-        progress.update(1)
+        i = 0
+        progress.update(task_ids[1], total=len(available_sources), refresh=True)
 
-    progress.close()
-    db.commit()
+        for name, source in available_sources.items():
+            progress.update(task_ids[0], description=f"[bold green]iCal-DHBWMannheim[/bold green] Adding {ical.name}")
+
+            ical_data, ical_hash = get_ical_data(source, "iCal-DHBWMannheim")
+            ical = ICalDHBWMannheim(name=name, source=source, data=ical_data, hash=ical_hash)
+            db.add(ical)
+            progress.update(task_ids[1], advance=1)
+            i += 1
+
+        db.commit()
+    except Exception as e:
+        progress.update(task_ids[0], description=f"[bold red]Error[/bold red]", visible=True)
+        progress.update(task_ids[1], description=f"[bold red]Error[/bold red]", visible=True)
+        print(e)
