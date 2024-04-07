1517f609529ce5166f16c33395a839957df20915
Kasimir Weilandt <Kasimir@Weilandt.it>
2024-04-07 13:40:10 +0200

    try fix localisation

--- Client/src/translations/TranslationConfig.tsx
+++ Client/src/translations/TranslationConfig.tsx
@@ -1,18 +1,34 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+
+import Backend from 'i18next-chained-backend';
+import AsyncStoragePlugin from 'i18next-react-native-async-storage';
+import XHR from 'i18next-xhr-backend';
+
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// add plural support
+import 'intl-pluralrules'
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'de',
     debug: true,
-    lng: 'de',
-    ns: ['common', 'anotherNamespace'],
+    fallbackLng: 'de',
+    supportedLngs: ['de', 'en'],
+    nonExplicitSupportedLngs: true,
+    ns: ['common'],
     defaultNS: 'common',
     backend: {
-      loadPath: 'locales/{{lng}}/{{ns}}.json',
+      backends: [
+        AsyncStoragePlugin, // primary
+        XHR,                // fallback
+      ],
+      backendOptions: [{
+        loadPath: 'locales/{{lng}}/{{ns}}.json',
+      }]
     },
     interpolation: {
       escapeValue: false,
