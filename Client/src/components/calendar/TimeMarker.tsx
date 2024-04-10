c86ba6925da8b794f5ddf43d1a167fee4fae38f2
Leonidas-maker <mrbeatbox@gmx.de>
2024-04-10 07:12:56 +0200

    fixed timemarker bug

--- Client/src/components/calendar/TimeMarker.tsx
+++ Client/src/components/calendar/TimeMarker.tsx
@@ -1,10 +1,11 @@
 // ~~~~~~~~~~~~~~~ Imports ~~~~~~~~~~~~~~~ //
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import 'nativewind';
 
 // ~~~~~~~~ Own components imports ~~~~~~~ //
-import { calculateMarkerPosition } from './CalendarCalculations';
+import { calculateMarkerPosition, calculateDayHeight } from './CalendarCalculations';
+import { set } from 'lodash';
 
 // ~~~~~~~~~~~~~~ Interfaces ~~~~~~~~~~~~~ //
 interface TimeMarkerProps {
@@ -21,6 +22,11 @@ interface TimeMarkerProps {
 // ====================================================== //
 const TimeMarker: React.FC<TimeMarkerProps> = ({ hoursContainerHeight, containerHeight, calendar }) => {
     // ====================================================== //
+    // ======================= States ======================= //
+    // ====================================================== //
+    const [showTimeMarker, setShowTimeMarker] = useState(true);
+
+    // ====================================================== //
     // =============== TimeMarker calculations ============== //
     // ====================================================== //
     // Gets the Marker Position
@@ -31,21 +37,34 @@ const TimeMarker: React.FC<TimeMarkerProps> = ({ hoursContainerHeight, container
         containerHeight: containerHeight,
     });
 
+    // Gets the Day Height
+    const dayHeight = calculateDayHeight(containerHeight, hoursContainerHeight);
+
+    // Sets the showTimeMarker state to false if the markerPosition is less than the dayHeight and should not be shown
+    useEffect(() => {
+        if (markerPosition < dayHeight) {
+            setShowTimeMarker(false);
+        } else {
+            setShowTimeMarker(true);
+        }
+    }, [markerPosition, dayHeight]);
+
     // ====================================================== //
     // ================== Return component ================== //
     // ====================================================== //
     return (
         <View className='absolute w-full'>
-
-            <View
-                className='bg-red-500 rounded-lg shadow-sm'
-                style={{
-                    position: 'absolute',
-                    top: markerPosition,
-                    width: '100%',
-                    height: 2,
-                }}>
-            </View>
+            {showTimeMarker &&
+                <View
+                    className='bg-red-500 rounded-lg shadow-sm'
+                    style={{
+                        position: 'absolute',
+                        top: markerPosition,
+                        width: '100%',
+                        height: 2,
+                    }}>
+                </View>
+            }
         </View>
     );
 };
