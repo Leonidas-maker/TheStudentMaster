5d8a21c450d918b8446597cd8a7e94d18d4de917
Leonidas-maker <mrbeatbox@gmx.de>
2024-04-29 15:32:12 +0200

    added navigator for account screens

--- Client/src/routes/LoadingStack.tsx
+++ Client/src/routes/LoadingStack.tsx
@@ -3,6 +3,7 @@ import Loading from '../screens/loading/Loading';
 import HomeBottomTabs from './HomeBottomTabs';
 import OverviewStack from './OverviewStack';
 import { StatusBar } from 'react-native';
+import CredentialStack from './CredentialStack';
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,7 @@ function LoadingStack() {
                 <Stack.Screen name="Loading" component={Loading} />
                 <Stack.Screen name="HomeBottomTabs" component={HomeBottomTabs} />
                 <Stack.Screen name="OverviewStack" component={OverviewStack} />
+                <Stack.Screen name="CredentialStack" component={CredentialStack} />
             </Stack.Navigator>
         </>
     );
