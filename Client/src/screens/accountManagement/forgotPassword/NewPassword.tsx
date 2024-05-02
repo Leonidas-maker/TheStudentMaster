a9b039c4f21f86cfc07f148fb8924d736ab601dd
Leonidas-maker <mrbeatbox@gmx.de>
2024-05-03 00:03:15 +0200

    added own component for textinputs

--- Client/src/screens/accountManagement/forgotPassword/NewPassword.tsx
+++ Client/src/screens/accountManagement/forgotPassword/NewPassword.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Text, View, ScrollView, TextInput, Keyboard, TouchableOpacity } from "react-native";
+import TextFieldInput from "../../../components/textInputs/TextFieldInput";
 
 const NewPassword: React.FC = () => {
 
@@ -15,8 +16,8 @@ const NewPassword: React.FC = () => {
             <ScrollView className='h-screen bg-primary'>
                 <View className="p-3 justify-center items-center">
                     <Text className="text-font_primary text-3xl font-bold text-center">Gib dein neues Passwort ein</Text>
-                    <TextInput className="bg-white w-3/4 h-10 rounded-xl border-2 bosrder-white focus:border-red-500 opacity-50" autoCapitalize="none" autoFocus={true} enterKeyHint="next" placeholder="Neues Passwort" autoComplete="new-password" />
-                    <TextInput className="bg-white w-3/4 h-10 rounded-xl border-2 bosrder-white focus:border-red-500 opacity-50" autoCapitalize="none" enterKeyHint="next" placeholder="Neues Passwort wiederholen" autoComplete="new-password" />
+                    <TextFieldInput autoCapitalize="none" autoFocus={true} enterKeyHint="next" placeholder="Neues Passwort" autoComplete="new-password" secureTextEntry={true} />
+                    <TextFieldInput autoCapitalize="none" enterKeyHint="done" placeholder="Neues Passwort wiederholen" autoComplete="new-password" secureTextEntry={true} />
                     <TouchableOpacity className="bg-white w-3/4 h-10 rounded-xl border-2 border-white focus:border-red-500 justify-center items-center mt-10">
                         <Text className="text-black">Passwort zurücksetzen</Text>
                     </TouchableOpacity>
