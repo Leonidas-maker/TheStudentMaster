9701c505da0a843237078ec649fa3b250e67c675
Leonidas-maker <mrbeatbox@gmx.de>
2024-05-11 15:28:53 +0200

    added logo to qr code

--- Client/src/screens/accountManagement/mfa/AddMfa.tsx
+++ Client/src/screens/accountManagement/mfa/AddMfa.tsx
@@ -8,8 +8,10 @@ import QRCode from "react-native-qrcode-svg";
 
 // TODO Implement get QR-Code
 // TODO Implement copy code to clipboard
+//TODO: Resize logo in image
 const AddMfa: React.FC = () => {
-  const sampleQRData = "https://example.com";
+  const sampleQRData = "otpauth://totp/TheShopMaster.com:schuetzeandreas.1%40web.de?secret=NIKBTQZM3AFR3Z3FCWSHOPGY53KJQ6YT&issuer=TheShopMaster.com";
+  let logo = require("../../../../public/logo/TheStudentMaster.png");
 
   return (
     <ScrollView className="h-screen bg-light_primary dark:bg-dark_primary">
@@ -19,6 +21,8 @@ const AddMfa: React.FC = () => {
         <QRCode
           value={sampleQRData}
           size={200}
+          logo={logo}
+          logoSize={50}
           color="black"
           backgroundColor="white"
         />
