22f79f15314b51be28637709e9b7c9eb3fe23046
chillkroete <info@wolf-andreas.eu>
2024-05-11 11:39:02 +0200

    change canteen update frequency to hourly

--- Server/main.py
+++ Server/main.py
@@ -83,6 +83,10 @@ async def repeated_task():
                                 update_all_native_calendars, progress, progress_id_update_calendar_dhbw_mannheim
                             )
                         )
+                        progress_id_canteen_menu = progress.add_task(
+                            "[bold green]Canteen[/bold green] Update Canteen Menus...", total=None
+                        )
+                        tasks.append(create_task(update_canteen_menus, progress, progress_id_canteen_menu))
                     else:
                         tasks.append(
                             create_task(
@@ -113,11 +117,6 @@ async def repeated_task():
                         )
                     )
 
-                # TODO @xxchillkroetexx: Update every 15 minutes necessary?
-                progress_id_canteen_menu = progress.add_task(
-                    "[bold green]Canteen[/bold green] Update Canteen Menus...", total=None
-                )
-                tasks.append(create_task(update_canteen_menus, progress, progress_id_canteen_menu))
                 native_calender_loops += 1
                 # await asyncio.gather(*tasks, return_exceptions=True)
                 progress.stop()
