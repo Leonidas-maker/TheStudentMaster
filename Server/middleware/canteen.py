4db4297a86a455eae62ad59245d0dfc99416ebca
chillkroete <info@wolf-andreas.eu>
2024-05-13 18:02:33 +0200

    small changes to the queried data for better readability

--- Server/middleware/canteen.py
+++ Server/middleware/canteen.py
@@ -325,25 +325,25 @@ def get_menu_for_canteen(db: Session, canteen_short_name: str, current_week_only
         current_week = 0
 
     try:
-        canteen_id = db.query(m_canteen.Canteen).filter_by(canteen_short_name=canteen_short_name).first().canteen_id
+        canteen = db.query(m_canteen.Canteen).filter_by(canteen_short_name=canteen_short_name).first()
     except AttributeError as e:
         print("Error while fetching canteen_id")
         print(e)
         return False
 
     try:
-        menus = db.query(m_canteen.Menu).filter_by(canteen_id=canteen_id).all()
+        menus = db.query(m_canteen.Menu).filter_by(canteen_id=canteen.canteen_id).all()
     except AttributeError as e:
         print("Error while fetching menu")
         print(e)
         return False
 
     return_value = dict()
-    return_value["canteen_name"] = menus[0].canteen.canteen_name if menus[0].canteen.canteen_name else None
+    return_value["canteen_name"] = canteen.canteen_name
     return_value["canteen_short_name"] = (
-        menus[0].canteen.canteen_short_name if menus[0].canteen.canteen_short_name else None
+        canteen.canteen_short_name if canteen.canteen_short_name else None
     )
-    return_value["image_url"] = menus[0].canteen.image_url if menus[0].canteen.image_url else None
+    return_value["image_url"] = canteen.image_url if canteen.image_url else None
     return_value["menu"] = list()
 
     if current_week_only:
