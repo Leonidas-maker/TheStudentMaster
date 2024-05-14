aa393879779e0239c1097187965e190b9e79d57f
Leonidas-maker <mrbeatbox@gmx.de>
2024-05-14 11:22:55 +0200

    updated to SDK 51 and fixed dependencies

--- Client/src/screens/accountManagement/login/Login.tsx
+++ Client/src/screens/accountManagement/login/Login.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { View, ScrollView, Keyboard, Pressable } from "react-native";
+import { View, ScrollView, Keyboard, Pressable, Alert } from "react-native";
 import TextFieldInput from "../../../components/textInputs/TextFieldInput";
 import DefaultButton from "../../../components/buttons/DefaultButton";
 import Heading from "../../../components/textFields/Heading";
 import TextButton from "../../../components/buttons/TextButton";
 import DefaultText from "../../../components/textFields/DefaultText";
+import axios from "axios";
 
+//! Still work in progress
 const Login: React.FC = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   const dismissKeyboard = () => {
     Keyboard.dismiss();
   };
@@ -14,6 +19,22 @@ const Login: React.FC = () => {
   const handleForgotPress = () => {
     console.log("Forgot password pressed");
   };
+
+  const handleLoginPress = async () => {
+    try {
+      const response = await axios.post("https://thestudentmaster.de/auth/login", {
+        username,
+        password,
+      });
+
+      if (response.data.access_token) {
+        Alert.alert("Login erfolgreich", "Access Token: " + response.data.access_token);
+      }
+    } catch (error) {
+      console.error("Login error: ", error);
+      Alert.alert("Login fehlgeschlagen", "Überprüfen Sie Ihre Anmeldeinformationen.");
+    }
+  }
 
   return (
     <Pressable onPress={dismissKeyboard}>
@@ -25,12 +46,16 @@ const Login: React.FC = () => {
             autoFocus={true}
             enterKeyHint="next"
             placeholder="Nutzername / Email"
+            value={username}
+            onChangeText={setUsername}
           />
           <TextFieldInput
             autoCapitalize="none"
             enterKeyHint="done"
             placeholder="Passwort"
             secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
           />
           <View className="w-3/4 justify-end items-end my-4">
             <TextButton
@@ -38,7 +63,7 @@ const Login: React.FC = () => {
               onPress={handleForgotPress}
             />
           </View>
-          <DefaultButton text="Anmelden" />
+          <DefaultButton text="Anmelden" onPress={handleLoginPress} />
         </View>
         <View className="h-full justify-center items-center">
           <View className="flex-row w-3/4 justify-center items-center">
