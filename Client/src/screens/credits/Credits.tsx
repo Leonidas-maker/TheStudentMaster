1aa8255c2f8514b91358de2b2466381b7bc29465
GitLab CI/CD <gitlab-ci@example.com>
2024-06-21 22:36:44 +0000

    Format code using Prettier

--- Client/src/screens/credits/Credits.tsx
+++ Client/src/screens/credits/Credits.tsx
@@ -11,17 +11,39 @@ const Credits: React.FC = () => {
             key={index}
             className="m-2 p-2 bg-light_secondary dark:bg-dark_secondary rounded-xl shadow-[rgba(0,0,0,0.5)_0px_5px_4px_0px]"
           >
-            <Text className="text-black dark:text-white">Department: {credit.department}</Text>
-            <Text className="text-black dark:text-white">Related To: {credit.relatedTo}</Text>
-            <Text className="text-black dark:text-white">Name: {credit.name}</Text>
-            <Text className="text-black dark:text-white">License Period: {credit.licensePeriod}</Text>
-            <Text className="text-black dark:text-white">Material: {credit.material}</Text>
-            <Text className="text-black dark:text-white">License Type: {credit.licenseType}</Text>
-            <Text className="text-black dark:text-white">Link: {credit.link}</Text>
-            <Text className="text-black dark:text-white">Remote Version: {credit.remoteVersion}</Text>
-            <Text className="text-black dark:text-white">Installed Version: {credit.installedVersion}</Text>
-            <Text className="text-black dark:text-white">Defined Version: {credit.definedVersion}</Text>
-            <Text className="text-black dark:text-white">Author: {credit.author}</Text>
+            <Text className="text-black dark:text-white">
+              Department: {credit.department}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Related To: {credit.relatedTo}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Name: {credit.name}
+            </Text>
+            <Text className="text-black dark:text-white">
+              License Period: {credit.licensePeriod}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Material: {credit.material}
+            </Text>
+            <Text className="text-black dark:text-white">
+              License Type: {credit.licenseType}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Link: {credit.link}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Remote Version: {credit.remoteVersion}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Installed Version: {credit.installedVersion}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Defined Version: {credit.definedVersion}
+            </Text>
+            <Text className="text-black dark:text-white">
+              Author: {credit.author}
+            </Text>
           </View>
         ))}
       </View>
