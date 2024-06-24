ff24311cf0bb751dc52d296fa1cd5b1106520e40
Leonidas-maker <mrbeatbox@gmx.de>
2024-06-24 22:09:35 +0200

    added more informations to some pages, changed version to 1.0.0

--- Client/src/screens/bugReport/BugReport.tsx
+++ Client/src/screens/bugReport/BugReport.tsx
@@ -1,21 +1,41 @@
 // ~~~~~~~~~~~~~~~ Imports ~~~~~~~~~~~~~~~ //
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Linking } from "react-native";
 
 // ~~~~~~~~ Own components imports ~~~~~~~ //
 import DefaultText from "../../components/textFields/DefaultText";
+import Heading from "../../components/textFields/Heading";
+import DefaultButton from "../../components/buttons/DefaultButton";
 
 // ====================================================== //
 // ====================== Component ===================== //
 // ====================================================== //
 const BugReport: React.FC = () => {
   // ====================================================== //
+  // =================== Press handlers =================== //
+  // ====================================================== //
+  const handleGitLabPress = () => {
+    Linking.openURL("https://gitlab.com/themastercollection/thestudentmaster");
+  };
+
+  // ====================================================== //
   // ================== Return component ================== //
   // ====================================================== //
   return (
     <ScrollView className="h-screen bg-light_primary dark:bg-dark_primary">
-      <View>
-        <DefaultText text="Welcome to the Bug Report page" />
+      <View className="px-5 py-5">
+        <View className="mb-5">
+          <Heading text="Du hast einen Fehler gefunden?" />
+        </View>
+        <View className="flex-1 m-5">
+          <View className="mb-3">
+            <DefaultText text="Erstelle ein Issue auf GitLab damit wir diesen beheben können." />
+          </View>
+          <DefaultText text="Gerne kannst du auch selber versuchen den Fehler zu beheben, da diese App ein Open Source Projekt ist!" />
+        </View>
+        <View className="content-center items-center">
+          <DefaultButton text="Zum GitLab Repository" onPress={handleGitLabPress} />
+        </View>
       </View>
     </ScrollView>
   );
--- Client/src/screens/support/Support.tsx
+++ Client/src/screens/support/Support.tsx
@@ -1,21 +1,39 @@
 // ~~~~~~~~~~~~~~~ Imports ~~~~~~~~~~~~~~~ //
 import React from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Linking } from "react-native";
 
 // ~~~~~~~~ Own components imports ~~~~~~~ //
 import DefaultText from "../../components/textFields/DefaultText";
+import Heading from "../../components/textFields/Heading";
+import DefaultButton from "../../components/buttons/DefaultButton";
+import TextButton from "../../components/buttons/TextButton";
 
 // ====================================================== //
 // ====================== Component ===================== //
 // ====================================================== //
 const Support: React.FC = () => {
   // ====================================================== //
+  // =================== Press handlers =================== //
+  // ====================================================== //
+  const handleMailPress = () => {
+    Linking.openURL("mailto:support@thestudentmaster.de");
+  };
+
+  // ====================================================== //
   // ================== Return component ================== //
   // ====================================================== //
   return (
     <ScrollView className="h-screen bg-light_primary dark:bg-dark_primary">
-      <View>
-        <DefaultText text="Welcome to the Support page" />
+      <View className="px-5 py-5">
+        <View className="mb-5">
+          <Heading text="Du benötigst Hilfe oder hast eine Frage?" />
+        </View>
+        <View className="flex-1 m-5">
+          <View className="mb-3">
+            <DefaultText text="Schreib uns bitte eine E-Mail an die unten stehende Adresse und wir werden versuchen dir so schnell wie möglich weiterzuhelfen." />
+          </View>
+          <TextButton text="support@thestudentmaster.de" onPress={handleMailPress} />
+        </View>
       </View>
     </ScrollView>
   );
